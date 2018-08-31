3f7143f6d72e8e01e7977d705f5bb5da3ac5f975
刘朋芳 <lpf_mart@163.com>
2018-08-31 14:25:23 +0800

    删除一个节点

--- 单链表/Program.cs
+++ 单链表/Program.cs
@@ -24,19 +24,26 @@ namespace 头插单链表
             node = new Node(12, node);
             node = new Node(13, node);
             node = new Node(14, node);
-            //Node j = node;
-            //while (j != null)
-            //{
-            //    Console.WriteLine(j.data);
-            //    j = j.next;
-            //}
+            for(int k = 20; k < 30; k++)
+            {
+                node = new Node(k, node);
+            }
             Console.WriteLine(node.ToString());
+            // 获取一个节点
+            Node.GetElement(node, 10);
+            // start 在第11的位置,插入一个节点
+            Node.InsertElement(node, 10, 99);
+            Console.WriteLine(node);
+            Node.DelElement(node,2);
+            Console.WriteLine(node);
+            // end 在第11的位置,
         }
+        
     }
     class Node
     {
         public readonly int data;
-        public readonly Node next;
+        public  Node next;
         public Node(int data) : this(data,null)
         {
         }
@@ -48,6 +55,72 @@ namespace 头插单链表
         public override string ToString()
         {
             return data.ToString() + " " + next?.ToString();
+        }
+        /// <summary>
+        /// 获取一个节点
+        /// </summary>
+        /// <param name="node">头指针</param>
+        /// <param name="i">节点位置</param>
+        /// <returns></returns>
+        public static int GetElement(Node node,int i)
+        {
+            // start 返回第十个元素的值
+            Node p = node;
+            int j = 1;
+            while (p != null && j < i)
+            {
+                p = p.next;
+                ++j;
+            }
+            if (p == null)
+            {
+                Console.WriteLine("error");
+                return 0;
+            }
+            Console.WriteLine(p.data);
+            return p.data;
+            // end 返回第十个元素的值
+        }
+        /// <summary>
+        /// 插入一个节点
+        /// </summary>
+        /// <param name="node">头指针</param>
+        /// <param name="loc">插入位置</param>
+        /// <param name="data">插入数据</param>
+        public static void InsertElement(Node node, int loc, int data)
+        {
+            Node s = new Node(data);
+            int j = 1;
+            while (node != null && j < loc - 1)
+            {
+                node = node.next;
+                ++j;
+            }
+            if (node == null)
+            {
+                throw new IndexOutOfRangeException("链表没这么长");
+            }
+            s.next = node.next;
+            node.next = s;
+        }
+        /// <summary>
+        /// 删除一个节点
+        /// </summary>
+        /// <param name="node"></param>
+        /// <param name="loc"></param>
+        public static void DelElement(Node node, int loc)
+        {
+            int j = 1;
+            while(node!=null && j < loc - 2)
+            {
+                node = node.next;
+                ++j;
+            }
+            if (node.next == null)
+            {
+                throw new IndexOutOfRangeException("链表没这么长");
+            }
+            node.next = node.next.next;
         }
     }
 }
