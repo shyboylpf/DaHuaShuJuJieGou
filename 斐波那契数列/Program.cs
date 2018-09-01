1e461b6a19391c57cb58c7107e03dfa97fbf525e
刘朋芳 <lpf_mart@163.com>
2018-09-01 11:20:06 +0800

    递归 菲波那切数列

---
+++ 斐波那契数列/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace 斐波那契数列
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 40; i++)
+            {
+                Console.WriteLine(Fbi(i));
+            }
+        }
+        public static int Fbi(int i)
+        {
+            if (i < 2)
+            {
+                return i;//== 0 ? 0 : 1;
+            }
+            return Fbi(i - 1) + Fbi(i - 2);
+        }
+
+    }
+
+}
