06862d8022e298900e2620ae405bbc10eb3a1eda
刘朋芳 <lpf_mart@163.com>
2018-09-02 14:23:54 +0800

    波兰表达式求职

---
+++ 波兰表达式/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace 波兰表达式
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> charStack = new Stack<string>();
+            string s = "931-3*+82/+";
+            int left;
+            int right;
+            foreach( var s1 in s)
+            {
+                if (!"+-*/".Contains(s1))
+                {
+                    charStack.Push(s1.ToString());
+                }
+                else
+                {
+                    right = int.Parse(charStack.Pop().ToString());
+                    left = int.Parse(charStack.Pop().ToString());
+                    switch (s1)
+                    {
+                        case '+':
+                            charStack.Push((left+right).ToString());
+                            break;
+                        case '-':
+                            charStack.Push((left - right).ToString());
+                            break;
+                        case '*':
+                            charStack.Push((left * right).ToString());
+                            break;
+                        case '/':
+                            charStack.Push((left / right).ToString());
+                            break;
+                    }
+                }
+            }
+
+            Console.WriteLine(charStack.Pop());
+        }
+    }
+}
