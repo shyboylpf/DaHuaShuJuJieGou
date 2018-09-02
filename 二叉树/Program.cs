558d9d14e374d5324571f072d839bb043134d1dc
刘朋芳 <lpf_mart@163.com>
2018-09-02 19:20:31 +0800

    二叉树 , 只会结构 , 并不会初始化

---
+++ 二叉树/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace 二叉树
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BiTNode root = new BiTNode();
+            Console.WriteLine("Hello World!");
+        }
+    }
+    /// <summary>
+    /// 二叉树的二叉链表实现
+    /// </summary>
+    class BiTNode
+    {
+        public int data; // 结点数据
+        public BiTNode lchild, rchild; // 左右孩子指针
+        public BiTNode()
+        {
+            data = default(int);
+            lchild = null;
+            rchild = null;
+        }
+        public BiTNode(int data)
+        {
+            this.data = data;
+            lchild = null;
+            rchild = null;
+        }
+        public BiTNode(int val, BiTNode lp, BiTNode rp)
+        {
+            data = val;
+            lchild = lp;
+            rchild = rp;
+        }
+        public BiTNode(BiTNode lp, BiTNode rp)
+        {
+            data = default(int);
+            lchild = lp;
+            rchild = rp;
+        }
+
+    }
+}
