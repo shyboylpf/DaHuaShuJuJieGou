4b1d3dcd904c862a4aceea0b98b5a1dad58ec8c5
刘朋芳 <lpf_mart@163.com>
2018-09-17 14:24:13 +0800

    图邻接表

---
+++ 图邻接表/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 图邻接表
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    class EdgeNode
+    {
+        int adjvex;
+        int weight;
+        EdgeNode next;
+    }
+    class VertexNode
+    {
+        char data;
+        EdgeNode firstedge;
+    }
+    class GraphAdjList
+    {
+        
+    }
+}
