1cd90968720e1b0628b0d42c828e338749496e7b
shyboylpf <lpf.mart@gmail.com>
2020-04-30 18:27:22 +0800

    值类型的整形互换 , 不需要临时变量

---
+++ 整数变量互换/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace 整数变量互换
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int x = 20;
+            int y = 20;
+            x ^= y;
+            y ^= x;
+            x ^= y;
+            Console.WriteLine(x + " " + y);
+        }
+    }
+}
