4b4cf74241c5480d129014a77a1438addfc67d77
shyboylpf <lpf.mart@gmail.com>
2020-04-30 17:21:03 +0800

    快速排序

---
+++ 快速排序/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace 快速排序
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int[] source = new int[10];
+            Random rd = new Random();
+            for (int i = 0; i < source.Length; i++)
+            {
+                source[i] = rd.Next(100);
+            }
+
+            var result = QuickSort(source);
+            Console.WriteLine(string.Join(',', result));
+        }
+
+        private static int[] QuickSort(int[] source)
+        {
+            _quickSort(ref source, 0, source.Length - 1);
+            return source;
+        }
+
+        private static void _quickSort(ref int[] arr, int low, int high)
+        {
+            if (low < high)
+            {
+                // pi is partitioning index,  arr[p] is now
+                // at right place
+                int pi = _position(arr, low, high);
+
+                _quickSort(ref arr, low, pi - 1);
+                _quickSort(ref arr, pi + 1, high);
+            }
+        }
+
+        private static int _position(int[] arr, int low, int high)
+        {
+            // index of smaller element
+            int i = low - 1;
+            // pivot
+            int pivot = arr[high];
+
+            for (int j = low; j < high; j++)
+            {
+                if (arr[j] <= pivot)
+                {
+                    i += 1;
+
+                    // exchage arr[i] , arr[j]
+                    arr[i] = arr[i] ^ arr[j];
+                    arr[j] = arr[j] ^ arr[i];
+                    arr[i] = arr[i] ^ arr[j];
+                }
+            }
+            // exchange arr[i+1],  arr[high]
+            arr[i + 1] = arr[i + 1] ^ arr[high];
+            arr[high] = arr[high] ^ arr[i + 1];
+            arr[i + 1] = arr[i + 1] ^ arr[high];
+
+            return i + 1;
+        }
+    }
+}
