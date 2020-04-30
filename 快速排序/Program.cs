bbd04a3a3c4cd6e05cfa1c2c1753253ceb250949
shyboylpf <lpf.mart@gmail.com>
2020-04-30 18:25:48 +0800

    修复异或引起的bug , 因为这儿是同一个变量 , 异或回出问题

--- 快速排序/Program.cs
+++ 快速排序/Program.cs
@@ -6,12 +6,12 @@ namespace 快速排序
     {
         private static void Main(string[] args)
         {
-            int[] source = new int[10];
-            Random rd = new Random();
-            for (int i = 0; i < source.Length; i++)
-            {
-                source[i] = rd.Next(100);
-            }
+            int[] source = new int[5] { 5, 4, 3, 2, 1 };
+            //Random rd = new Random();
+            //for (int i = 0; i < source.Length; i++)
+            //{
+            //    source[i] = rd.Next(1, 100);
+            //}
 
             var result = QuickSort(source);
             Console.WriteLine(string.Join(',', result));
@@ -29,14 +29,14 @@ namespace 快速排序
             {
                 // pi is partitioning index,  arr[p] is now
                 // at right place
-                int pi = _position(arr, low, high);
+                int pi = _position(ref arr, low, high);
 
                 _quickSort(ref arr, low, pi - 1);
                 _quickSort(ref arr, pi + 1, high);
             }
         }
 
-        private static int _position(int[] arr, int low, int high)
+        private static int _position(ref int[] arr, int low, int high)
         {
             // index of smaller element
             int i = low - 1;
@@ -48,17 +48,22 @@ namespace 快速排序
                 if (arr[j] <= pivot)
                 {
                     i += 1;
-
-                    // exchage arr[i] , arr[j]
-                    arr[i] = arr[i] ^ arr[j];
-                    arr[j] = arr[j] ^ arr[i];
-                    arr[i] = arr[i] ^ arr[j];
+                    if (i != j)
+                    {
+                        // exchage arr[i] , arr[j]
+                        arr[i] ^= arr[j];
+                        arr[j] ^= arr[i];
+                        arr[i] ^= arr[j];
+                    }
                 }
             }
-            // exchange arr[i+1],  arr[high]
-            arr[i + 1] = arr[i + 1] ^ arr[high];
-            arr[high] = arr[high] ^ arr[i + 1];
-            arr[i + 1] = arr[i + 1] ^ arr[high];
+            if (i + 1 != high)
+            {
+                // exchange arr[i+1],  arr[high]
+                arr[i + 1] ^= arr[high];
+                arr[high] ^= arr[i + 1];
+                arr[i + 1] ^= arr[high];
+            }
 
             return i + 1;
         }
